f63723e850a508cfe2c662457df84639440636d4
Denis Ah-Kang <denis@w3.org>
2016-04-01 09:24:31 +0400

    new action handlers: labelled and unlabelled

--- sync.py
+++ sync.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 import sys
 import os
+import cgi
 import subprocess
 import shutil
 import json
 import ConfigParser
+import getpass
 import hmac
 import itertools
 import lockfile
@@ -110,6 +112,8 @@ def process_pull_request(config, data, user_is_authorised):
 
     action_handlers = {"opened": pull_request_opened,
                        "reopened": pull_request_opened,
+                       "labeled": pull_request_labeled,
+                       "unlabeled": pull_request_unlabeled,
                        "closed": end_mirror,
                        "synchronize": sync_mirror}
     action_handlers[action](base_path, data["pull_request"]["number"], user_is_authorised)
@@ -117,6 +121,12 @@ def process_pull_request(config, data, user_is_authorised):
 def pull_request_opened(base_path, number, user_is_authorised):
     if user_is_authorised:
         start_mirror(base_path, number, user_is_authorised)
+
+def pull_request_labeled(base_path, number, user_is_authorised):
+    pass
+
+def pull_request_unlabeled(base_path, number, user_is_authorised):
+    pass
 
 def start_mirror(base_path, number, user_is_authorised):
     if not PullRequestCheckout.exists(base_path, number):
@@ -246,7 +256,7 @@ def get_open_pull_request_numbers(config):
 def setup(config):
     create_master(config)
     for number in get_open_pull_request_numbers(config):
-        PullRequestCheckout.create(config["base_path"], number)
+        pull_request = PullRequestCheckout.create(config["base_path"], number)
     register_events(config)
 
 def register_events(config):
